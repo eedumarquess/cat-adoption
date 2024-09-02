443985d6261c9a05b50ff1e436f6c6ba62ce6ff6
Edu <eedumarquess@gmail.com>
2024-09-02 15:39:06 -0300

    FEAT: create config for tailwind with font, colors and size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,35 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    './nuxt.config.{js,ts}',  // Corrigido para incluir o arquivo de configuração do Nuxt.js
   ],
   theme: {
-    extend: {},
+    colors: {
+      'main': '#4e2096',
+      'primary': '#26203b',
+      'secundary': '#4d4e4e',
+      'stroke': '#dfe4e8',
+      'placeholder': '#b2b2b2',
+      'danger': '#e03130',
+      'sucess': '#04d58b',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        'xs': '0.75rem',   // 12px
+        'sm': '0.875rem',  // 14px
+        'base': '1rem',    // 16px
+        'lg': '1.5rem',    // 24px
+        'xl': '2rem',      // 32px
+        '2xl': '2.5rem',   // 40px
+      },
+    },
   },
   plugins: [],
 }
